36de7f62e4244355b9db820c470aa9dcc80aa6cc
Mohit <20bma048@nith.ac.in>
2022-08-13 17:12:23 +0530

    updated

--- ArrayAndString/43_Chocolate Distribution Problem.cpp
+++ ArrayAndString/43_Chocolate Distribution Problem.cpp
@@ -1,1 +1,20 @@
-// https://practice.geeksforgeeks.org/problems/chocolate-distribution-problem3825/1
+// // https://practice.geeksforgeeks.org/problems/chocolate-distribution-problem3825/1
+// //Sliding Window Approach
+// class Solution{
+//     public:
+//     long long findMinDiff(vector<long long> a, long long n, long long m){
+//     //code
+    
+//     sort(a.begin(),a.end());
+//     int i =0;
+//     int j = m-1;
+//     int ans=INT_MAX;
+//     while(j<n){
+//         int diff = a[j]-a[i];
+//         ans = min(ans,diff);
+//         i++;
+//         j++;
+//     }
+//     return ans;
+//     }   
+// };
